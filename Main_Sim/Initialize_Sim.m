1a186ef82c3e9c12ee2873c96b2007d1130a2ec9
anyajewell <139726398+anyajewell@users.noreply.github.com>
2026-09-08 18:58:23 -0400

    Ready to run Fresnel sweep at higher Omega

--- Main_Sim/Initialize_Sim.m
+++ Main_Sim/Initialize_Sim.m
@@ -1,8 +1,8 @@
 function [consts, sim, laser, frame, mirror, outputs, toggles, gain_medium] = Initialize_Sim(N_F)
     
-    % Settings 
+    % Settings/Toggles
     track_centers = true;
-    gain_switch = false; % gain ON or OFF
+    gain_switch = false; % gain medium present or not
     outputs_switch = true;
     videoplot_frequency = 'every mirror'; % 'every step', 'every mirror', or 'never/none'
     finish_line = 'convergence'; % 'convergence' or 'RTs'
@@ -10,9 +10,11 @@ function [consts, sim, laser, frame, mirror, outputs, toggles, gain_medium] = In
     resize_grid = false;
     convergence_def = 'general'; % 'TEM00' or 'general'
     initial_profile = 'cavity mode'; % 'cavity mode' or 'general'
+    %save_location = 'local'; % 'local' or 'git'
     toggles.track_centers = track_centers; toggles.gain_switch = gain_switch; toggles.outputs_switch = outputs_switch; 
     toggles.videoplot_frequency = videoplot_frequency; toggles.finish_line = finish_line; toggles.absorbing_mask = absorbing_mask;
     toggles.resize_grid = resize_grid; toggles.convergence_def = convergence_def; toggles.initial_profile = initial_profile;
+    %toggles.save_location = save_location;
 
     % Constants
     c = 3e8; % [m/s]
@@ -26,29 +28,31 @@ function [consts, sim, laser, frame, mirror, outputs, toggles, gain_medium] = In
     sim.Lx = Lx; sim.N = N; sim.dx = dx;
     
     % Interlink frame settings
-    Omega = 0.001; % rotational velocity, [rad/sec]
+    Omega = 0.002; % rotational velocity, [rad/sec]
     accel = 1e-5; % transverse acceleration, [m/s^2]
     v0 = 0; % starting transverse velocity, [m/s]
     frame.Omega = Omega; frame.accel = accel; frame.v0 = v0;
     
     % Simulation settings
-    L = 150e3; % cavity length, [m]
-    %D1 = 1;
+    %L = 150e3; % cavity length, [m]
+    Ld = 1064*1e-9; % Laser wavelength, [m]
+    D1 = 1;
+    L = (D1/2)^2 / (N_F*Ld); % calculate cavity length from Fresnel number
     Z0 = -L/2; % starting location, arbitrary, anywhere within the cavity [m]
     Nz = 1e2; % number of steps in one pass across the cavity (1/2 a round trip)
     dz = L/Nz; % step size, sign determines initial direction [m]
+    tau = 2*L/c; % round-trip time, [s]
     t0 = 0; t(1) = t0;
     dt = abs(dz/c); % time step, [s]
     tmax = dt*Nz; % max time, [s]
     z = linspace(Z0, L, Nz); % evaluatory locations within cavity
-    sim.Z0 = Z0; sim.L = L; sim.Nz = Nz; sim.dz = dz; sim.t0 = t0; sim.t = t; sim.dt = dt; sim.tmax = tmax; sim.z = z;
+    sim.Z0 = Z0; sim.L = L; sim.Nz = Nz; sim.dz = dz; sim.t0 = t0; sim.t = t; sim.dt = dt; sim.tmax = tmax; sim.z = z; sim.tau = tau;
     RTs = 500; % number of round trips to take, user set (represents a max if finish_line 'convergence' is on)
     %RTs = Set_Max_RTs(sim, consts, sampling_time); % to be used with PCAC
     sim.RTs = RTs;
     
     % Mirrors
-    Ld = 1064*1e-9; % Laser wavelength, [m]
-    D1 = 2*sqrt(N_F*L*Ld);
+    %D1 = 2*sqrt(N_F*L*Ld);
     %D1 = .8; % large size to reduce clipping, [m]
     D2 = D1; % diameter of mirror 2, [m]
     Rc1 = L; % radius of curvature for mirror 1, [m]
